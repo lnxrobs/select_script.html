1df369a9bb47173ad86cfb9fcf3212868b9ee87c
Robson Dariva <146855243+lnxrobs@users.noreply.github.com>
2024-12-23 15:52:11 -0300

    Update Script com JS e SendKeys.bat

--- Script com JS e SendKeys.bat
+++ Script com JS e SendKeys.bat
@@ -2,3 +2,18 @@ echo off
 SET SendKeys=CScript //nologo //
 E:JScript "%~F0" 
 
+TIMEOUT /t 3 
+%SendKeys% "null"
+%SendKeys% "{null}"
+%SendKeys% "null"
+%SendKeys% "null"
+%SendKeys% "{null}"
+
+GOTO :EOF 
+
+@end 
+// JScript section 
+var WshShell = 
+Escript.CreateObject("WScript.Shell");
+WshShell.SendKeys(WScript.Arguments
+(0));0
